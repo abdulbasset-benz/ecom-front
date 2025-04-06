3af38f51dbeef37a55963a037b0425fe6ae47224
abdulbasset-benz <benz.abdelbasset@gmail.com>
2025-04-06 21:17:13 +0100

    setup a basic routing and navbar using react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,20 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import api from "./api/api";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./auth/Home";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
 
 function App() {
-  const [count, setCount] = useState(5);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-red-500 uppercase font-extrabold hover:text-green-500 cursor-pointer">
-        Vite + React
-      </h1>
-      <div className="card">
-        --pfgfffffg{" "}
-        <button onClick={() => setCount((count) => count + 2)}>
-          ]]]]]] count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
     </>
   );
 }
